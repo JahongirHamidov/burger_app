119d8ae01842737621fd519f6baf139e77e82485
JahongirHamidov <hamidovjahongir444@gmail.com>
2021-01-12 17:23:37 +0500

    13.5 form changes

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -81,12 +81,23 @@ class ContactData extends Component {
     }
 
     render () {
+        const formElementsArray = []
+        for(let key in this.state.orderForm){
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         let form = (
             <form>
-                <Input inputtype='input'  type="text" name="name" placeholder="Your Name" />
-                <Input inputtype='input'  type="email" name="email" placeholder="Your Mail" />
-                <Input inputtype='input'  type="text" name="street" placeholder="Street" />
-                <Input inputtype='input'  type="text" name="postal" placeholder="Postal Code" />
+                {formElementsArray.map(formElement=>(
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType} 
+                        elementConfig={formElement.config.elementConfig} 
+                        value={formElement.config.elementConfig.value}    
+                    />
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
